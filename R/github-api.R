de68c3261d9df5ca47405d9276aa985094d4a438
Natalia Mladentseva <natashka.ml@gmail.com>
2023-01-28 02:54:16 +0000

    moved functions around

--- R/github-api.R
+++ R/github-api.R
@@ -1,3 +1,32 @@
+#' Quick Git
+#' Stage all, commit, and push to github all with one function.
+#' @param commit_msg Git Commit Message
+#' @export
+quick_git <- function(commit_msg = NULL){
+  changes <- gert::git_status()
+  n <- nrow(changes) # if(n == 0) stop("No")
+  usethis::ui_info("{n} unstaged files")
+  if(n > 0){
+    # files <- paste(changes$file, collapse = "', '")
+    usethis::ui_info("{usethis::ui_path(changes$file)}")
+    # git_add()/git_rm() #e.g., git_add(c("dev/", "man/"))
+    gert::git_add(".")
+    usethis::ui_done("Files staged")
+  }
+
+  if(is.null(commit_msg))
+    stop("You need to include a commit message with your commit!")
+  usethis::ui_info("Commit Message: {usethis::ui_field(commit_msg)}")
+  gert::git_commit(commit_msg) #now commit
+  usethis::ui_done("Git commit")
+
+  gert::git_push() #and push!
+  usethis::ui_done("Changes pushed!")
+
+  usethis::ui_info("Here's a log of your most recent commits")
+  return(gert::git_log())
+}
+
 
 # Search Github -----------------------------------------------------------
 #' search_github
@@ -62,7 +91,7 @@ search_github <- function(my_query, max = 100, wait = 10) {
 # debugonce(search_github)
 
 
-# fetch_ ------------------------------------------------------------------
+# fetch ------------------------------------------------------------------
 #' fetch_repos
 #' @param username github username chr
 #' @param .limit default Inf
